00a6089fb36e87613fa0eed710869b6bc59e3de7
JabenChou <zhoujabn@126.com>
2024-01-14 15:36:17 +0800

    github

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const OWNER = "AIBrain";
+export const REPO = "ChatGPT-AIBrain";
+export const REPO_URL = `https://aibrain.wiki`;
+export const ISSUE_URL = `https://aibrain.wiki`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
